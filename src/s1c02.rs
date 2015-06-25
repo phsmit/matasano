55338c6689909f61e66d94ae0c7592ec86f93ad8
Peter Smit <peter@smitmail.eu>
2015-06-25 09:29:05 +0300

    More idiomatic version of challenge 2

--- src/s1c02.rs
+++ src/s1c02.rs
@@ -4,8 +4,8 @@ use rustc_serialize::hex::{FromHex,ToHex};
 pub fn fixed_xor(l: &str, r: &str) -> String {
     let mut l = l.from_hex().unwrap();
     let r = r.from_hex().unwrap();
-    for i in 0..l.len() {
-        l[i] ^= r[i];
+    for (le, re) in l.iter_mut().zip(r.iter()) {
+        *le ^= *re;
     }
     return l.to_hex();
 }
